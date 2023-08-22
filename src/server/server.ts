61103a1881956a7b30e3bd54daa0500e28d95fc8
jeefyang <jeefyjl@gmail.com>
2023-08-22 11:01:23 +0800

    调整node的监听内容

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,11 @@ import express from "express"
 import { createExpressMiddleware } from '@trpc/server/adapters/express';
 import { appRouter } from './router';
 import cors from "cors"
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
+
+import { JConfigType } from "../type"
 
 console.log("start!")
 const app = express()
@@ -16,6 +21,14 @@ app.use('/trpc', createExpressMiddleware({
 app.get('/test', async (_req, res, _next) => {
     res.status(200).set({ "Content-Type": "text/html" }).end("helloworld")
 })
+
+if (import.meta.env.PROD) {
+    const josnUrl = path.join(path.dirname(fileURLToPath(import.meta.url)), "config.jsonc")
+    const jsonStr = fs.readFileSync(josnUrl, "utf-8")
+    const configjson: JConfigType = eval(`(${jsonStr})`)
+    app.listen(configjson.vue_build_port)
+    console.log("正在监听:", configjson.vue_build_port)
+}
 
 
 
