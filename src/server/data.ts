2707d882a5b4acb081ae721f11899636b8fbc66b
jeefyang <jeefyjl@gmail.com>
2024-03-14 15:15:05 +0800

    调整读取config.jsonc的位置

--- src/server/data.ts
+++ src/server/data.ts
@@ -3,7 +3,10 @@ import path from "path"
 import { fileURLToPath } from "url"
 
 import { JConfigType } from "../type"
+let josnUrl = "./config.jsonc"
+if (!fs.existsSync(josnUrl)) {
+    josnUrl = path.join(path.dirname(fileURLToPath(import.meta.url)), "config.jsonc")
+}
 
-const josnUrl = path.join(path.dirname(fileURLToPath(import.meta.url)), "config.jsonc")
 const jsonStr = fs.readFileSync(josnUrl, "utf-8")
 export const configjson: JConfigType = eval(`(${jsonStr})`)
